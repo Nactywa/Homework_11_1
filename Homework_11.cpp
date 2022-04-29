87de52c65a52e31529be07b7d73ccf825e5e00f7
Nastya Zbukareva <nactywa@mail.ru>
2022-04-29 10:52:30 +0300

    Add project files.

---
+++ Homework_11.cpp
@@ -0,0 +1,38 @@
+// HelloWorld1!.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+//  Вызывает функцию std::cout
+void print()
+{
+    // Потому что хотим использовать стандартную библиотеку
+    // для печати и консоли и не использовать другие
+    //  какие либо библиотеки
+
+    std::cout << "Hello Skillbox!\n";
+}
+
+/*
+Присваиваем значение переменым и печатаем hello world!
+*/
+int main()
+{
+    int x = 100;
+    int y = x + 100;
+
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    int mult = x * y;
+    int random = 1002;
+    random = 1002;
+
+    std::cout << random;
+
+}
